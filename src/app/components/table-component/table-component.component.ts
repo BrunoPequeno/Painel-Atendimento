ffd9b9620c138867e1df39709837f6be3263ac62
Bruno Pequeno <bru.pequeno@gmail.com>
2023-10-27 15:49:45 -0300

    feat: Creating table-component

--- src/app/components/table-component/table-component.component.ts
+++ src/app/components/table-component/table-component.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'src/app/service/firebase.service';
 
 @Component({
   selector: 'app-table-component',
   templateUrl: './table-component.component.html',
   styleUrls: ['./table-component.component.css']
 })
-export class TableComponentComponent {
+export class TableComponentComponent implements OnInit {
+  voos: any[] = [];
 
+  constructor(private firebaseService: FirebaseService) { }
+
+  ngOnInit() {
+    this.firebaseService.getVoos().subscribe(data => {
+      this.voos = data;
+    });
+  }
 }
