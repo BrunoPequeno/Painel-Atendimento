433edea76bb6bde55454da3dceb8212a8b643d5e
Bruno Pequeno <bru.pequeno@gmail.com>
2023-10-27 13:19:30 -0300

    feat: Finishing header

--- src/app/components/header-component/header-component.component.ts
+++ src/app/components/header-component/header-component.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header-component',
   templateUrl: './header-component.component.html',
   styleUrls: ['./header-component.component.css']
 })
-export class HeaderComponentComponent {
+export class HeaderComponentComponent implements OnInit {
+  dataAtual: string = "0";
 
+  ngOnInit() {
+    this.atualizarDataHora();
+
+    setInterval(() => {
+      this.atualizarDataHora();
+    }, 1000);
+  }
+
+  atualizarDataHora() {
+    const agora = new Date();
+    const data = agora.toLocaleDateString();
+    const hora = agora.toLocaleTimeString();
+    this.dataAtual = `${data} ${hora}`;
+  }
 }
