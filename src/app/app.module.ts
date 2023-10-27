152140bef692f5d9c5829670d7c82ae8897af8bc
Bruno Pequeno <bru.pequeno@gmail.com>
2023-10-27 12:23:57 -0300

    feat: Creating table-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableComponentComponent } from './components/table-component/table-component.component';
+import { HeaderComponentComponent } from './components/header-component/header-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TableComponentComponent,
+    HeaderComponentComponent
   ],
   imports: [
     BrowserModule
